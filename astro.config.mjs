66ab07a3a0d2a9e187b414fb0038733d53048cd8
not-diba <elvis.madiba@gmail.com>
2025-12-12 22:35:01 +0300

    feat: Improving model loading performance

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,31 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-
 import tailwindcss from "@tailwindcss/vite";
-
 import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
+    optimizeDeps: {
+      include: ["three"],
+      exclude: [
+        "three/examples/jsm/loaders/GLTFLoader",
+        "three/examples/jsm/loaders/DRACOLoader",
+      ],
+    },
+    ssr: {
+      noExternal: ["three"],
+    },
+    build: {
+      rollupOptions: {
+        output: {
+          manualChunks: {
+            "three-core": ["three"],
+          },
+        },
+      },
+    },
   },
-
   integrations: [icon()],
 });
-
